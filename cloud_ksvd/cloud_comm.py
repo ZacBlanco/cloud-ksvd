7052201a82a35d68299885c5392a3e4adb19a512
Zac Blanco <zblanco@apache.org>
2016-11-28 01:39:12 -0500

    [#5] - Update get behavior to remove data from comm
    After retrieving data from the communicator it is "cleaned" by setting
    the entries to None after they are accessed.
    
    A unit test was added to ensure this behavior

--- cloud_ksvd/cloud_comm.py
+++ cloud_ksvd/cloud_comm.py
@@ -8,6 +8,7 @@ import struct
 TAG_SIZE = 4
 SEQ_SIZE = 2
 
+
 def get_mtu():
     '''Attempts to return the MTU for the network by finding the min of the 
     first hop MTU and 576 bytes. i.e min(MTU_fh, 576)
@@ -23,6 +24,7 @@ def get_mtu():
     '''
     return 576
 
+
 def check_port(port):
     '''Checks if a port is valid.
 
@@ -36,12 +38,13 @@ def check_port(port):
         bool: Only will return True. Anything invalid will result in a ValueError
 
     '''
-    if type(port) != int: # Ensure we're dealing with real ports
+    if type(port) != int:  # Ensure we're dealing with real ports
         raise TypeError("port must be an int between 0 and 65535")
     elif port < 0 or port > 65535:
         raise ValueError("port must be an int between 0 and 65535")
     else:
         return True
+
 
 def get_payload(payload):
     '''Take data payload and return a byte array of the object. Should be 
@@ -61,6 +64,7 @@ def get_payload(payload):
     data = json.dumps(payload, separators=[':', ',']).encode('utf-8')
     return data
 
+
 def decode_payload(payload):
     '''Takes a byte array and converts it into an object using ``json.loads``
 
@@ -73,6 +77,7 @@ def decode_payload(payload):
 
     data = json.loads(payload.decode('utf-8'), separator([':', ',']))
     return data
+
 
 class Communicator:
     '''This is a threaded class interface designed to send and receive messages
@@ -97,9 +102,9 @@ class Communicator:
      must also add a 'tag' to the data. It should be a 4-byte
      long identifier. For strings this is limited to 4 characters.
       Anything longer than 4 is truncated
-    
+
       - ``comm.send('IP_ADDRESS', my_bytes, 'tag1')``
-    
+
 
     3. After sending, there's nothing else for the client to do'
     4. When the packet reaches the other end, each packet is received and
@@ -111,7 +116,7 @@ class Communicator:
     As simple as that!
 
     Notes:
-    
+
     - A limitation (dependent upon python implementation) is that threaded
      there may only be a single python thread running at one time due to GIL
       (Global Interpreter Lock)
@@ -123,7 +128,7 @@ class Communicator:
 
     - Data segments which have not been reconstructed lie within
      ``self.tmp_data``. Reconstructed data is within ``self.data_store``
-         
+
     Constructor Docs
 
     Args:
@@ -140,28 +145,34 @@ class Communicator:
         if protocol not in ['TCP', 'UDP']:
             raise ValueError('Protocol must be one of "TCP" or "UDP"')
         else:
-            self.protocol = protocol # The protocol an upper case string 'TCP' or 'UDP'
-        
+            self.protocol = protocol  # The protocol an upper case string 'TCP' or 'UDP'
+
         if protocol == 'TCP':
-            raise NotImplementedError('TCP not yet implemented. Please use UDP instead')
+            raise NotImplementedError(
+                'TCP not yet implemented. Please use UDP instead')
 
         # Check and create sockets
         if send_port != None and check_port(send_port):
             self.send_port = send_port
             self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        
+
         if check_port(listen_port):
             self.listen_port = listen_port
             self.listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            if send_port == None: # If the port and socket were not set
+            if send_port == None:  # If the port and socket were not set
                 self.send_port = self.listen_port
-                self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                self.send_sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM)
 
-        self.listen_sock.setblocking(False) # Set to nonblocking in order to get our threaded server to work :) (We should investigate the performance impact of this)
+        # Set to nonblocking in order to get our threaded server to work :) (We
+        # should investigate the performance impact of this)
+        self.listen_sock.setblocking(False)
         # Set the defaults for the thread and listening object
         self.is_open = True
         self.listen_thread = None
-        self.is_listening = False # This tells the listening thread whether or not it need to continue looping to receive messages
+        # This tells the listening thread whether or not it need to continue
+        # looping to receive messages
+        self.is_listening = False
         self.tmp_data = {}
         self.data_store = {}
         self.mtu = get_mtu()
@@ -173,10 +184,10 @@ class Communicator:
 
         Args:
             N/A
-        
+
         Returns:
             N/A
-        
+
         Raises:
             BrokenPipeError: If close was already called previously.
 
@@ -187,9 +198,8 @@ class Communicator:
             self.send_sock.close()
             self.is_open = False
         else:
-            raise BrokenPipeError('Cannot close. Sockets were previously closed.')
-    
-
+            raise BrokenPipeError(
+                'Cannot close. Sockets were previously closed.')
 
     def listen(self):
         '''Start listening on port ``self.listen_port``. Creates a new thread where the socket will be created
@@ -202,13 +212,14 @@ class Communicator:
 
         '''
         if self.is_open != True:
-            raise BrokenPipeError('Cannot listen. Sockets were previously closed.')
+            raise BrokenPipeError(
+                'Cannot listen. Sockets were previously closed.')
 
-        if self.listen_thread == None: # Create thread if not already created
-            self.listen_thread = threading.Thread(target=self.__run_listen__, args=(self.listen_sock, '', self.listen_port))
+        if self.listen_thread == None:  # Create thread if not already created
+            self.listen_thread = threading.Thread(
+                target=self.__run_listen__, args=(self.listen_sock, '', self.listen_port))
             self.is_listening = True
             self.listen_thread.start()
-        
 
     def __run_listen__(self, _sock, host, port):
         '''Worker method for the threaded listener in order to retrieve incoming messages
@@ -222,16 +233,15 @@ class Communicator:
             N/A
 
         '''
-        
+
         _sock.bind((host, port))
 
         while self.is_listening:
             try:
-                data, addr = _sock.recvfrom(1024) # Receive at max 1024 bytes
+                data, addr = _sock.recvfrom(1024)  # Receive at max 1024 bytes
                 self.receive(data, addr[0])
             except BlockingIOError:
                 pass
-
 
         _sock.close()
 
@@ -240,17 +250,18 @@ class Communicator:
 
         Args:
             seq_num(int): The packet's sequence number
-            seq_total(int): The
+            seq_total(int): The total number of sequence packets to be sent
+            tag (str/ ytes): A string or bytes object to encode as the data tag
 
         Returns:
             bytearray: A bytearray with the metadata
         '''
         if type(seq_total) != int or type(seq_num) != int:
-            raise TypeError("Sequence number and total must be integer") 
+            raise TypeError("Sequence number and total must be integer")
         packet = bytearray()
         packet += struct.pack('H', seq_total)
         packet += struct.pack('H', seq_num)
-        packet += tag.encode('utf-8')[0:4]
+        packet += str(tag).encode('utf-8')[0:4]
         return packet
 
     def create_packets(self, data, tag):
@@ -275,13 +286,13 @@ class Communicator:
         Args:
             data (bytes): The data as a string which is meant to be sent to its destination
             tag (str): A tag. Only the first 4 bytes (chars) are added as the tag.
-        
+
         Returns
             list: A list containing the payload for the packets which should be sent to the destination.
         '''
         packets = []
-        max_payload = self.mtu - 60 - 8 # conservative estimate to prevent IP fragmenting
-        metadata_size = 8 # 8 bytes         
+        max_payload = self.mtu - 60 - 8  # conservative estimate to prevent IP fragmenting
+        metadata_size = 8  # 8 bytes
         data_size = len(data)
         max_data = max_payload - metadata_size
 
@@ -294,24 +305,23 @@ class Communicator:
             payload += data
             packets.append(payload)
         else:
-            total_packets = math.floor(data_size/max_payload)
+            total_packets = math.floor(data_size / max_payload)
             tp1 = total_packets - 1
-            for i in range(total_packets): # [0, total_packets-1]
+            for i in range(total_packets):  # [0, total_packets-1]
                 p1 = self.build_meta_packet(i, total_packets, tag)
 
-                # Slice data into ~500 byte packs 
-                d1 = data[i*max_data:(i+1)*max_data]
+                # Slice data into ~500 byte packs
+                d1 = data[i * max_data:(i + 1) * max_data]
                 p1 += d1
                 packets.append(p1)
-            # Build the  final packet 
+            # Build the  final packet
             p1 = self.build_meta_packet(total_packets, total_packets, tag)
             min_bound = (total_packets) * max_data
             d1 = data[min_bound:]
             p1 += d1
             packets.append(p1)
-            
-        return packets
 
+        return packets
 
     def send(self, ip, data, tag):
         '''Send a chunk of data with a specific tag to an ip address. The packet will be automatically chunked into N packets where N = ceil(bytes/(MTU-68))
@@ -325,19 +335,18 @@ class Communicator:
             bool: True if all packets were created and sent successfully.
         '''
 
-        # As simple as just creating the packets and sending each one individually
+        # As simple as just creating the packets and sending each one
+        # individually
         if self.is_open != True:
             raise BrokenPipeError('Socket was already closed by user')
 
         ret = True
         packets = self.create_packets(data, tag)
         for packet in packets:
-            if (self.send_sock.sendto(packet, (ip, self.send_port)) < 0 ):
+            if (self.send_sock.sendto(packet, (ip, self.send_port)) < 0):
                 ret = False
         return ret
 
-        
-    
     def get(self, ip, tag):
         '''Get a key/tag value from the data store. 
 
@@ -346,22 +355,22 @@ class Communicator:
         The ``self.data_store`` object has the following structure:
 
         .. code-block:: javascript
-        
+
             {
                 ip_address: {
                     tag_1: data,
                     tag_2: data2
                 }
             }
-        
-        
+
+
         Args:
             ip (str): The ip address of the host we wish get data from
             tag (str): The data tag for the message which is being received
 
         Returns:
             bytes: ``None`` if complete data is not found, Otherwise if found will return the data
-        
+
         '''
         data = None
         if ip not in self.data_store:
@@ -370,6 +379,7 @@ class Communicator:
             data = None
         else:
             data = self.data_store[ip][tag]
+            self.data_store[ip][tag] = None
 
         return data
 
@@ -385,12 +395,10 @@ class Communicator:
             N/A
 
         '''
-        if self.listen_thread !=  None:
+        if self.listen_thread != None:
             self.is_listening = False
             self.listen_thread.join()
             self.listen_thread = None
-
-
 
     def receive(self, data, addr):
         '''Take a piece of data received over the socket and processes the data and attempt to combine packet sequences together, passing them to the data store when ready.
@@ -398,7 +406,7 @@ class Communicator:
         ``self.tmp_data`` is an object with the structure
 
         .. code-block:: javascript
-        
+
             {
                 ip_address: {
                     tag_1: {
@@ -428,7 +436,7 @@ class Communicator:
         '''
         if addr not in self.tmp_data:
             self.tmp_data[addr] = {}
-        
+
         # disassemble the packet
         seq_total = struct.unpack('H', data[0:2])[0]
         seq_num = struct.unpack('H', data[2:4])[0]
@@ -442,15 +450,17 @@ class Communicator:
             self.tmp_data[addr][data_tag]['seq_total'] = seq_total
 
         if seq_total != self.tmp_data[addr][data_tag]['seq_total'] or self.tmp_data[addr][data_tag]['seq_total'] == None:
-            # If the tag existed, make sure the sequence total is equal to the current, otherwise throw away any packets we've already collected
+            # If the tag existed, make sure the sequence total is equal to the
+            # current, otherwise throw away any packets we've already collected
             self.tmp_data[addr][data_tag]['seq_total'] = seq_total
             self.tmp_data[addr][data_tag]['packets'] = {}
 
         self.tmp_data[addr][data_tag]['packets'][seq_num] = dat
 
-        num_packets = len(self.tmp_data[addr][data_tag]['packets']) 
+        num_packets = len(self.tmp_data[addr][data_tag]['packets'])
         # print(self.tmp_data[addr][data_tag]['packets'].keys())
-        if  num_packets == seq_total + 1: # seq_total is max index of 0-index based list.
+        # seq_total is max index of 0-index based list.
+        if num_packets == seq_total + 1:
             # Reassmble the packets in order
             reassembled = bytes()
             for i in range(num_packets):
@@ -460,12 +470,3 @@ class Communicator:
             self.data_store[addr][data_tag] = reassembled
             self.tmp_data[addr][data_tag]['packets'] = {}
             self.tmp_data[addr][data_tag]['seq_total'] = {}
-
-
-
-            
-
-        
-        
-
-
